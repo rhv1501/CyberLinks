c5b894cb4f391cea72ab5d9762307100342b1ffb
rhv1501 <129303708+rhv1501@users.noreply.github.com>
2025-02-04 19:52:03 +0530

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -9,26 +9,47 @@ export async function POST(Request) {
     const collection = db.collection("urls");
     const doc = await collection.findOne({ shorturl: body.shorturl });
     if (doc) {
-      return Response.json({
+      return new Response(JSON.stringify({
         err: true,
         message: "Short URL already exists",
         error: e,
+      }), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      });
+      return new Response(JSON.stringify({
+        err: true,
+        message: "Short URL already exists",
+      }), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
       });
     }
-    const result = await collection.insertOne({
-      url: body.url,
-      shorturl: body.shorturl,
-    });
-    return Response.json({
+
+    return new Response(JSON.stringify({
       err: false,
       message: "Short URL generated",
       shorturl: body.shorturl,
+    }), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
     });
   } catch (e) {
-    return Response.json({
+    return new Response(JSON.stringify({
       err: true,
       message: "Error connecting to database",
       error: e,
+    }), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
     });
   }
 }
