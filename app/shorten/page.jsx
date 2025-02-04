1ff6964fb4b7d7e0af1fe726c78a039f0e01d60a
rhv1501 <129303708+rhv1501@users.noreply.github.com>
2025-02-04 21:33:55 +0530

    Update page.jsx for cors

--- app/shorten/page.jsx
+++ app/shorten/page.jsx
@@ -13,7 +13,7 @@ const page = () => {
 
     try {
       const res = await fetch("https://cybe-links.vercel.app/api/generate", {
-        mode: "no-cors",
+        mode: "cors",
         method: "POST",
         headers: {
           "Content-Type": "application/json",
